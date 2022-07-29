820b6a6bbf903bebccbe5089366c577784dad5bb
Shak49 <shakfeizi@gmali.com>
2022-07-29 10:40:53 -0500

    project file added

---
+++ CoffeeOrdering_Exp/CoffeeOrdering_Exp/CoffeeOrdering_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrdering_ExpApp.swift
+//  CoffeeOrdering_Exp
+//
+//  Created by Shak Feizi on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrdering_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
